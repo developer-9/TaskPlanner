c4ba8d1e20fb679170c38915f5d7adcdf7f48f83
developer-9 <falcon_hmktc_9@icloud.com>
2023-02-19 12:06:25 +0900

    init

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Taisei Sakamoto on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
